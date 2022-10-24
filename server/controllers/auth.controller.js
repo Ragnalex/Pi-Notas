12f1a76a332f0c952cda96b8c0dd20bb0a7aab6c
Miguel Gómez Gómez <66704572+Momo-Gomez@users.noreply.github.com>
2022-10-23 22:14:41 -0300

    Auth admin

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,33 @@
 const Profesor = require("../models/profesor.model");
 const Alumno = require("../models/alumno.model");
+const Administrador = require("../models/admin.model");
 const bcrypt = require("bcrypt");
 //LOGIN
+
+const AdminLogin = async (req, res) => {
+  try {
+    const admin = await Administrador.findOne({
+      correo: req.body.correo.toLowerCase(),
+    });
+    if (!admin) {
+      return res.send("Correo no encontrado en el sistema");
+    }
+    if (admin.contrasena === "-1") {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPass = await bcrypt.hash(req.body.contrasena, salt);
+      admin.contrasena = hashedPass;
+      await admin.save();
+    }
+    if (await bcrypt.compare(req.body.contrasena, admin.contrasena)) {
+      return res.status(200).json(admin);
+    } else {
+      return res.send("Contraseña ingresada es incorrecta."); //En caso de no coincidir contrasena, envia este mensaje para que aparezca en el frontend
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 const AlumnoLogin = async (req, res) => {
   try {
     const alum = await Alumno.findOne({
@@ -35,4 +61,5 @@ const ProfesorLogin = async (req, res) => {
 module.exports = {
   AlumnoLogin,
   ProfesorLogin,
+  AdminLogin,
 };
