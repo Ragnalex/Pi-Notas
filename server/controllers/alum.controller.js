14706b0838de8f90d7f846be5b84611c86d1f782
Miguel Gómez Gómez <66704572+Momo-Gomez@users.noreply.github.com>
2022-11-22 12:05:34 -0300

    Update alum.controller.js

--- server/controllers/alum.controller.js
+++ server/controllers/alum.controller.js
@@ -1,9 +1,11 @@
 const Curso = require("../models/curso.model");
 
-
+// VerAsignaruturas ()
+// Busca el curso y retorna las asignaturas con todos sus datos
+// returns 200 [asignaturas]
+// returns 500 []
 const VerAsignaturas = async (req,res) => {
     try {
-      console.log(req.body.idCurso)
       const curso = await Curso.findById(req.body.idCurso).populate("asignaturas");
       res.status(200).json(curso.asignaturas);
     } catch (err) {
